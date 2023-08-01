a116ecc626f9deba726d70fff32a16d32295cb25
MartinKotac <mkotevski65@gmail.com>
2023-08-01 10:12:31 +0200

    spring initializr

---
+++ src/test/java/com/example/securityPlayground/SecurityPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityPlayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
